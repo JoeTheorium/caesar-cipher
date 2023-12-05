e22b6eef888570b53e73443d6cfacc51d1a228b8
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-12-05 19:35:31 -0300

    Add animation & Finish project.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const inputOriginal = document.getElementById("input-original");
 const cipher = document.getElementById("cipher");
 const result = document.getElementById("result");
 const range = document.getElementById("range");
 
-const shifMessage = () => {
+const shiftMessage = () => {
   const wordArray = [...inputOriginal.value.toUpperCase()];
   // alert(wordArray);
   printChar(0, wordArray);
@@ -15,17 +15,32 @@ const printChar = (currentLetterIndex, wordArray) => {
   inputOriginal.value = inputOriginal.value.substring(1);
   const spanChar = document.createElement('span');
   result.appendChild(spanChar);
-  const charArray = wordArray[currentLetterIndex];
-  spanChar.innerHTML = alphabet.includes(charArray) ? alphabet[(alphabet.indexOf(charArray) + parseInt(range.value)) % alphabet.length] : charArray;
-  printChar(currentLetterIndex + 1, wordArray); // Recursive
+
+  animateChar(spanChar).then(() => {
+    const charArray = wordArray[currentLetterIndex];
+    spanChar.innerHTML = alphabet.includes(charArray) ? alphabet[(alphabet.indexOf(charArray) + parseInt(range.value)) % alphabet.length] : charArray;
+    printChar(currentLetterIndex + 1, wordArray); // Recursive
+  });
+}
+
+const animateChar = spanChar => {
+  let letterChanges = 0;
+  return new Promise(resolve => {
+    const interval = setInterval(() => {
+      spanChar.innerHTML = alphabet[Math.floor(Math.random() * alphabet.length)];
+      letterChanges++;
+      if (letterChanges === 3) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 50);
+  });
 }
 
 const submit = e => {
   e.preventDefault();
   result.innerHTML = '';
-  shifMessage();
+  shiftMessage();
 }
 
 cipher.onsubmit = submit;
-
-
